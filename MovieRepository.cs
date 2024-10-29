59381d05eb8d6327a906f8d44fbf4941f13a6a65
SnehaAkkenapally <sneha.akkenapally11@gmail.com>
2024-10-28 23:05:33 -0400

    Add files via upload

---
+++ MovieRepository.cs
@@ -0,0 +1,27 @@
+﻿using ApplicationCore.Contracts.Repository;
+using ApplicationCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    public class MovieRepository : IMovieRepository
+    {
+       public List<Movie> GetMovies(  )
+        {
+            List<Movie> movies = new List<Movie>();
+            for (int i = 0; i < 10; i++)
+            {
+                Movie movie = new Movie();
+                movie.name = "moviename" + i;
+                movie.year = 2024;
+                movies.Add( movie );
+            }
+            return movies;
+        }
+        
+    }
+}
